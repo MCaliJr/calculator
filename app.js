6b38e7c708eb8f66d0aaa7f3e9deb3b06c324136
mcalijr <martink.moc@gmail.com>
2021-10-06 23:47:03 +0200

    Visual site done. Looking crisp in my opinion. Tomorrow is a JS day haha

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 console.log("working hihi");
+
+function adjustCalcBackground(x) {
+  let offsetHeight = document.querySelector(".output").offsetHeight;
+  if (x.matches) {
+    // Adjust calculator background based on input field size
+    document.getElementById("glass-board").style.height =
+      580 + offsetHeight + "px";
+    document.getElementById("glass-board").style.top = "50%";
+    document.getElementById("glass-board").style.transform =
+      "translate(-50%, -50%)";
+  } else {
+    document.getElementById("glass-board").style.height =
+      580 + offsetHeight + "px";
+    document.getElementById("glass-board").style.top = "0px";
+    document.getElementById("glass-board").style.transform = "translate(-50%)";
+  }
+}
+
+let x = window.matchMedia("(min-height: 701px)");
+adjustCalcBackground(x); // Call listener function at run time
+x.addListener(adjustCalcBackground); // Listener function firing on state changes
